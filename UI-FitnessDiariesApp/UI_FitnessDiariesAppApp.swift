4c468d445e04887c48598dd9426acf856db7864a
Valentin Malissen <keisuke.tk.fd@gmail.com>
2020-07-22 20:47:40 -0400

    Initial Commit

---
+++ UI-FitnessDiariesApp/UI_FitnessDiariesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  UI_FitnessDiariesAppApp.swift
+//  UI-FitnessDiariesApp
+//
+//  Created by Valentin Malissen on 2020-07-22.
+//
+
+import SwiftUI
+
+@main
+struct UI_FitnessDiariesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
